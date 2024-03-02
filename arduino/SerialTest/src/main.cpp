b8e442d800b8dee709394746078e17d1dcc98614
Jacob Browning <jacob.browning161@gmail.com>
2024-03-02 02:53:50 -0500

    Bigger Messages

--- arduino/SerialTest/src/main.cpp
+++ arduino/SerialTest/src/main.cpp
@@ -1,25 +1,36 @@
 #include <Arduino.h>
-String command;
+
+String command = "";
+
+void processCommand(String);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
-
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  if (Serial.available() >= 2) { // Check if at least 2 bytes are available
-    char firstChar = Serial.read(); // Read the first character
-    char secondChar = Serial.read(); // Read the second character
+  while (Serial.available() > 0) { // Check if any characters are available
+    char incomingChar = Serial.read(); // Read the incoming character
+    
+    if (incomingChar == '.') { // Check if it's the end of the message
+      // Process the received command
+      processCommand(command);
+      
+      // Reset the command string for the next message
+      command = "";
+    } else {
+      // Append the character to the command string
+      command += incomingChar;
+    }
+  }
+}
 
-    if (firstChar == 'O' && secondChar == 'N') { // Check if it's "ON"
-      // Do something when "ON" is received
-      digitalWrite(13, HIGH);
-    }
-    if (firstChar == 'O' && secondChar == 'F') { // Check if it's "ON"
-      // Do something when "ON" is received
-      digitalWrite(13, LOW);
-    }
+void processCommand(String cmd) {
+  // Check the received command and take appropriate action
+  if (cmd == "ON") {
+    digitalWrite(13, HIGH);
+  } else if (cmd == "OFF") {
+    digitalWrite(13, LOW);
   }
 }
