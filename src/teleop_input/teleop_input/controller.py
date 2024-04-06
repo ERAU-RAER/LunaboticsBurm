d454c6f00b969687559352b27076aa334487af90
Jacob Browning <jacob.browning161@gmail.com>
2024-04-06 18:31:31 -0400

    Remapped to D-pad

--- src/teleop_input/teleop_input/controller.py
+++ src/teleop_input/teleop_input/controller.py
@@ -24,13 +24,13 @@ class JoyToCmdVel:
 
         twist_msg = Twist()
 
-        twist_msg.linear.x = msg.axes[5]  # Assuming Y-axis of the joystick for forward/backward motion
+        twist_msg.linear.x = msg.axes[1]  # Assuming Y-axis of the joystick for forward/backward motion
 
-        twist_msg.angular.z = msg.axes[4]  # Assuming X-axis of the joystick for rotation
+        twist_msg.angular.z = msg.axes[0]  # Assuming X-axis of the joystick for rotation
 
-        twist_msg.linear.z = msg.axes[1]
+        twist_msg.linear.z = msg.axes[5]
   
-        twist_msg.angular.y = msg.axes[0]
+        twist_msg.angular.y = msg.axes[4]
 
 
         # Publish Twist message
