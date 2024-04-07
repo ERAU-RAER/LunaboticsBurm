63e682f632801a8b97f91467525a0e1b7bc1c0c7
Jacob Browning <jacob.browning161@gmail.com>
2024-04-06 21:46:52 -0400

    testing new launch file

--- src/teleop_input/setup.py
+++ src/teleop_input/setup.py
@@ -1,5 +1,6 @@
+import os
 from setuptools import find_packages, setup
-
+from glob import glob
 package_name = 'teleop_input'
 
 setup(
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+    (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
