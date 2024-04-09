1e4c4b145aa077a42e2390bc0fe365669ba3da44
Evan Airhart <101517055+Sgail7@users.noreply.github.com>
2024-04-09 00:24:23 -0400

    Update drivetrain.cpp

--- src/arduino_serial_package/arduino_files/drivetrain/src/drivetrain.cpp
+++ src/arduino_serial_package/arduino_files/drivetrain/src/drivetrain.cpp
@@ -12,15 +12,25 @@
 #define PWM_BITS 8             // microcontroller's PWM pin resolution. Arduino Mega 2560 is using 8 bits (0-255)
 
 // Define pin variables
-int motor1_pin = 11;
-int motor2_pin = 10;
-int motor3_pin = 9;
-int motor4_pin = 5;
+#define motor1_pin 13
+#define motor2_pin 12
+#define motor3_pin 11
+#define motor4_pin 10
 
-int dir1_pin = 22;
-int dir2_pin = 23;
-int dir3_pin = 24;
-int dir4_pin = 25;
+#define dir1_pin 22
+#define dir2_pin 23
+#define dir3_pin 28
+#define dir4_pin 29
+
+#define stop1_pin 24
+#define stop2_pin 25
+#define stop3_pin 30
+#define stop4_pin 31
+
+#define brake1_pin 26
+#define brake2_pin 27
+#define brake3_pin 32
+#define brake4_pin 33
 
 DD_Kinematics Kinematics(MOTOR_MAX_RPM, WHEEL_DIAMETER, FR_WHEELS_DIST, LR_WHEELS_DIST, PWM_BITS);
 
@@ -36,6 +46,24 @@ void setup()
   pinMode(motor2_pin, OUTPUT);
   pinMode(motor3_pin, OUTPUT);
   pinMode(motor4_pin, OUTPUT);
+
+  // Initialize dir pins as outputs (Pull low)
+  pinMode(dir1_pin, OUTPUT);
+  pinMode(dir2_pin, OUTPUT);
+  pinMode(dir3_pin, OUTPUT);
+  pinMode(dir4_pin, OUTPUT);
+
+  // Initialize Freewheel pins as inputs (Floating)
+  pinMode(stop1_pin, INPUT);
+  pinMode(stop2_pin, INPUT);
+  pinMode(stop3_pin, INPUT);
+  pinMode(stop4_pin, INPUT);
+
+  // Initialize Brake pins as outputs (Pull low)
+  pinMode(brake1_pin, OUTPUT);
+  pinMode(brake2_pin, OUTPUT);
+  pinMode(brake3_pin, OUTPUT);
+  pinMode(brake4_pin, OUTPUT);
 } 
 
 void loop()
@@ -69,16 +97,52 @@ void loop()
   //find required rpm for each motor to obtain the desired values
   rpm = Kinematics.getRPM(linear_vel_x, ang_vel_z);
 
+  //Disable motor 1 if RPM is zero
+  if (rpm.motor1 == 0) {
+    digitalWrite(stop1_pin, LOW);
+  }
+  else {
+    // Enable motor 1 if RPM is not zero
+    digitalWrite(stop1_pin, HIGH);
+  }
+
+  // Disable motor 2 if RPM is zero
+  if (rpm.motor2 == 0) {
+    digitalWrite(stop2_pin, LOW);
+  }
+  else {
+    // Enable motor 2 if RPM is not zero
+    digitalWrite(stop2_pin, HIGH);
+  }
+
+    // Disable motor 2 if RPM is zero
+  if (rpm.motor3 == 0) {
+    digitalWrite(stop3_pin, LOW);
+  }
+  else {
+    // Enable motor 2 if RPM is not zero
+    digitalWrite(stop3_pin, HIGH);
+  }
+  
+    // Disable motor 2 if RPM is zero
+  if (rpm.motor4 == 0) {
+    digitalWrite(stop4_pin, LOW);
+  }
+  else {
+    // Enable motor 2 if RPM is not zero
+    digitalWrite(stop4_pin, HIGH);
+  }
+
   // Something like what we'd want to do for motor speed feedback. Demo is below to let the motors work
   // int motor1_feedback = rpm.encoder1;
   // int motor2_feedback = rpm.encoder2;
   // int motor3_feedback = rpm.encoder3;
   // int motor4_feedback = rpm.encoder4;
 
-  int motor1_feedback = rpm.motor1;
-  int motor2_feedback = rpm.motor2;
-  int motor3_feedback = rpm.motor3;
-  int motor4_feedback = rpm.motor4;
+  float motor1_feedback = rpm.motor1;
+  float motor2_feedback = rpm.motor2;
+  float motor3_feedback = rpm.motor3;
+  float motor4_feedback = rpm.motor4;
 
   DD_Kinematics::velocities vel;
 
