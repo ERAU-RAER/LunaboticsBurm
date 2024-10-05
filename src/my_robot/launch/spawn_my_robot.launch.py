c3a8e0732b4094d254ad05dcd026162053b5c4a4
Jacob Browning <62108812+jake161@users.noreply.github.com>
2024-10-05 15:46:57 -0400

    Commented out broken code

--- src/my_robot/launch/spawn_my_robot.launch.py
+++ src/my_robot/launch/spawn_my_robot.launch.py
@@ -18,36 +18,36 @@ def generate_launch_description():
             executable='spawn_entity.py',
             arguments=['-file', urdf_file, '-entity', 'my_robot'],
             output='screen'
-        ),
+        )#,
 
-        Node(
-            package='controller_manager',
-            executable='ros2_control_node',
-            parameters=[config_file],
-            output='screen'
-        ),
+        # Node(
+        #     package='controller_manager',
+        #     executable='ros2_control_node',
+        #     parameters=[config_file],
+        #     output='screen'
+        # ),
 
-        Node(
-            package='teleop_twist_keyboard',
-            executable='teleop_twist_keyboard',
-            prefix='alacritty -e',
-            remappings=[('/cmd_vel', '/skid_steer/cmd_vel')],
-            output='screen'
-        ),
+        # Node(
+        #     package='teleop_twist_keyboard',
+        #     executable='teleop_twist_keyboard',
+        #     prefix='alacritty -e',
+        #     remappings=[('/cmd_vel', '/skid_steer/cmd_vel')],
+        #     output='screen'
+        # ),
 
-        # Spawner for the skid_steer controller
-        Node(
-            package='controller_manager',
-            executable='spawner.py',
-            arguments=['skid_steer_controller'],
-            output='screen'
-        ),
+        # # Spawner for the skid_steer controller
+        # Node(
+        #     package='controller_manager',
+        #     executable='spawner.py',
+        #     arguments=['skid_steer_controller'],
+        #     output='screen'
+        # ),
 
-        # Spawner for the joint state broadcaster
-        Node(
-            package='controller_manager',
-            executable='spawner.py',
-            arguments=['joint_state_broadcaster'],
-            output='screen'
-        )
+        # # Spawner for the joint state broadcaster
+        # Node(
+        #     package='controller_manager',
+        #     executable='spawner.py',
+        #     arguments=['joint_state_broadcaster'],
+        #     output='screen'
+        # )
     ])
